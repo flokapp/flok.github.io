d451a2dbf0f12d7503e2c23e07c2a362f6d28216
shtabnoy <denis.shtabnoy@gmail.com>
2021-10-31 01:15:39 +0200

    test local expo with session search

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-window.location.href = 'flok:/path/to';
+const sessionSearch = window.location.search;
+if (sessionSearch) {
+  // window.location.href = 'flok:/path/to/app';
+  window.location.href = 'exp://192.168.10.146:19000' + sessionSearch;
+}
